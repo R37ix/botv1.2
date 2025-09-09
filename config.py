6eabe8a2cc6f4bff71a3ac728eab69aea08ac074
Nikolas Cage <167676988+R37ix@users.noreply.github.com>
2025-09-09 21:51:48 +0300

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-BOT_TOKEN = ""  # Замените на токен от @BotFather
-ADMIN_IDS = []  # Сюда добавьте ID администраторов
+BOT_TOKEN = "7897606994:AAEa1E8BwCfJjXczL6war8p53MIcGNBEwLk"  # Замените на токен от @BotFather
+ADMIN_IDS = [5419664849]  # Сюда добавьте ID администраторов
 
 DB_NAME = "class_bot.db"
+
