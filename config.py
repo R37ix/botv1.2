92adfd32f443c05d7de2983520b7fc3e7c65517e
Nikolas Cage <167676988+R37ix@users.noreply.github.com>
2025-09-16 21:03:46 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-BOT_TOKEN = ""
-ADMIN_IDS = []  # ID администраторов
-
+BOT_TOKEN = "7897606994:AAEa1E8BwCfJjXczL6war8p53MIcGNBEwLk"
+ADMIN_IDS = [5419664849]  # ID администраторов
 DB_NAME = "class_bot.db"
