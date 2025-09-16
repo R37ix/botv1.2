0d36de3ffed6888b223241b94705ca70c6784d67
Nikolas Cage <167676988+R37ix@users.noreply.github.com>
2025-09-16 21:10:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-BOT_TOKEN = "7897606994:AAEa1E8BwCfJjXczL6war8p53MIcGNBEwLk"
-ADMIN_IDS = [5419664849]  # ID администраторов
+BOT_TOKEN = ""
+ADMIN_IDS = []  # ID администраторов
+
 DB_NAME = "class_bot.db"
